9c71782972d3c7ca2e98f8a2713dca1a04c0dce0
Edward Kim <itgma1231@gmail.com>
2024-07-22 12:51:50 -0400

    .addEventListener() on form submission; check URL validity

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("hello")
+const form = document.getElementById('link-form');
+const input = document.getElementById('link-input');
+const errorMsg = document.getElementById('error-msg');
+
+const validateURL = (url) => {
+  const urlRegex =
+    /^(?:(?:https?|ftp):\/\/)?(?:(?:[a-zA-Z0-9_\-\.]+\.)+[a-zA-Z]{2,}|localhost|(?:[0-9]{1,3}\.){3}[0-9]{1,3})(?::\d+)?(?:\/(?:[^\s\/#\?]{0,255}(?:\/[^\s\/#\?]{0,255})*)*)?(?:\?[^\s+&@#/%=]*=[^\s+&@#/%=]*(?:&[^\s+&@#/%=]*=[^\s+&@#/%=]*)*)?(?:#[^\s+]*?)?$/;
+  return urlRegex.test(url);
+};
+
+const formSubmit = (event) => {
+  event.preventDefault();
+
+  if (input.value === '') {
+    input.classList.add('border-red');
+    errorMsg.innerHTML = 'Please enter a URL';
+  } else if (!validateURL(input.value)) {
+    input.classList.add('border-red');
+    errorMsg.innerHTML = 'Please enter a valid URL';
+  } else {
+    input.classList.remove('border-red');
+    errorMsg.innerHTML = '';
+  }
+};
+
+form.addEventListener('submit', formSubmit);
